bd2377d999c646f4d85c26dba212b22a30cab358
Kazuki Yokoyama <kmyokoyama@inf.ufrgs.br>
2016-08-09 19:17:52 -0300

    Set web installation to false by default

--- attributes/web.rb
+++ attributes/web.rb
@@ -4,7 +4,7 @@ default['zabbix']['web']['install_method']  = 'apache'
 default['zabbix']['web']['fqdn']            = node['fqdn']
 default['zabbix']['web']['aliases']         = ['zabbix']
 default['zabbix']['web']['port']            = 80
-default['zabbix']['web']['install']         = true
+default['zabbix']['web']['install']         = false 
 default['apache']['mpm']                    = "prefork"
 
 default['zabbix']['web']['php']['fastcgi_listen'] = '127.0.0.1:9000' # only applicable when using php-fpm (nginx)
