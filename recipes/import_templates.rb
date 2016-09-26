0ae6abdb57bc369ec6641192d98c6a19d1653146
Kazuki Yokoyama <kmyokoyama@inf.ufrgs.br>
2016-09-26 16:59:10 -0300

    Improve Template files handling

--- recipes/import_templates.rb
+++ recipes/import_templates.rb
@@ -23,15 +23,37 @@ connection_info = {
   :password => zabbix_server['zabbix']['web']['password']
 }
 
-files_default = ::File.join(Chef::Config['file_cache_path'], 'cookbooks/zabbix/files/default')
+#files_default = ::File.join(Chef::Config['file_cache_path'], 'cookbooks/zabbix/files/default/zabbix-templates')
+#
+#node['zabbix']['server']['template_files'].each do |file|
+#    filepath = ::File.join(files_default, file)
+#	if ::File.exist?(filepath)
+#		configuration_data = ::File.read(filepath)
+#	else
+#		Chef::Log.info('Template file #{filepath} does not exist. Skipping.')
+#		next
+#	end
+#
+#    zabbix_configuration node['zabbix']['web']['fqdn'] do
+#      server_connection connection_info
+#      source configuration_data
+#      action :import
+#    end
+#end
 
-node['zabbix']['server']['template_files'].each do |file|
-    filepath = ::File.join(files_default, file)
-    configuration_data = ::File.read(filepath)
+node['zabbix'['server']['template_files'].each do |file|
+	tmp_path = ::File.join('/tmp/zabbix/templates', file)
+	cookbook_file tmp_path do
+		source "zabbix-templates/#{file}"
+		owner node['zabbix']['login']
+		group node['zabbix']['group']
+		mode '0400'
+	end.run_action(:create)
 
-    zabbix_configuration node['zabbix']['web']['fqdn'] do
-      server_connection connection_info
-      source configuration_data
-      action :import
-    end
+	configuration_data = ::File.read(tmp_path)
+	zabbix_configuration node['zabbix']['web']['fqdn'] do
+    server_connection connection_info
+    source configuration_data
+    action :import
+	end
 end
